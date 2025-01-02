e3a88b2d29b46829a93145875bfd0b41424e8456
ShNik <kostya.nikitenko04@gmail.com>
2025-01-02 22:20:45 +0300

    лаб1 №8 Создан класс, содержащий краткую версию данных исходного класса

--- Client.py
+++ Client.py
@@ -3,11 +3,10 @@ import json
 
 class ClientBase:
 
-    def __init__(self, client_id: int = None, name: str = '', ownership_type: str = '', address: str = '', phone: str = ''):
+
+    def __init__(self, client_id: int = None, name: str = '', phone: str = ''):
         self.client_id = client_id
         self.name = name
-        self.ownership_type = ownership_type
-        self.address = address
         self.phone = phone
 
     @staticmethod
@@ -17,14 +16,6 @@ class ClientBase:
     @staticmethod
     def validate_name(name: str) -> bool:
         return bool(name and re.match(r"^[a-zA-Zа-яА-ЯёЁ\s-]+$", name))
-
-    @staticmethod
-    def validate_ownership_type(ownership_type: str) -> bool:
-        return bool(ownership_type and re.match(r"^[a-zA-Zа-яА-ЯёЁ\s-]+$", ownership_type))
-
-    @staticmethod
-    def validate_address(address: str) -> bool:
-        return bool(address and isinstance(address, str))
 
     @staticmethod
     def validate_phone(phone: str) -> bool:
@@ -38,7 +29,7 @@ class ClientBase:
     @client_id.setter
     def client_id(self, value: int):
         if not self.validate_client_id(value):
-            raise ValueError("Неподходящий ID")
+            raise ValueError("Invalid client ID.")
         self.__client_id = value
 
     @property
@@ -48,8 +39,83 @@ class ClientBase:
     @name.setter
     def name(self, value: str):
         if not self.validate_name(value):
-            raise ValueError("Неподходящее имя")
+            raise ValueError("Invalid name.")
         self.__name = value
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, value: str):
+        if not self.validate_phone(value):
+            raise ValueError("Invalid phone number.")
+        self.__phone = value
+
+    @classmethod
+    def create_new_client(cls, client_id: int, name: str, phone: str):
+        return cls(client_id=client_id, name=name, phone=phone)
+
+    @staticmethod
+    def from_string(client_str: str):
+        parts = client_str.split(',')
+        if len(parts) != 3:
+            raise ValueError("The string must contain 3 comma-separated elements.")
+        client_id = int(parts[0].strip())
+        name, phone = map(str.strip, parts[1:])
+        return ClientBase(client_id, name, phone)
+
+    @classmethod
+    def from_json(cls, json_str: str):
+        data = json.loads(json_str)
+        return cls(
+            client_id=data.get('client_id'),
+            name=data['name'],
+            phone=data['phone']
+        )
+
+    def to_json(self) -> str:
+        return json.dumps({
+            'client_id': self.client_id,
+            'name': self.name,
+            'phone': self.phone
+        }, ensure_ascii=False, indent=4)
+
+    def to_dict(self) -> dict:
+        return {
+            'client_id': self.client_id,
+            'name': self.name,
+            'phone': self.phone
+        }
+
+    def __str__(self):
+        return (f"Client {self.name}, ID: {self.client_id}, Phone: {self.phone}")
+
+    def __eq__(self, other):
+        if not isinstance(other, ClientBase):
+            return False
+        return self.phone == other.phone
+
+
+class Client(ClientBase):
+
+    def __init__(self, client_id: int = None, name: str = '', ownership_type: str = '',
+                 address: str = '', phone: str = ''):
+        super().__init__(client_id, name, phone)
+        self.ownership_type = ownership_type
+        self.address = address
+
+    @staticmethod
+    def validate_ownership_type(ownership_type: str) -> bool:
+        return bool(ownership_type and re.match(r"^[a-zA-Zа-яА-ЯёЁ\s-]+$", ownership_type))
+
+    @staticmethod
+    def validate_address(address: str) -> bool:
+        return bool(address and isinstance(address, str))
+
+    @staticmethod
+    def validate_contract_details(contract_details: str) -> bool:
+        return bool(contract_details and isinstance(contract_details, str))
 
     @property
     def ownership_type(self):
@@ -58,7 +124,7 @@ class ClientBase:
     @ownership_type.setter
     def ownership_type(self, value: str):
         if not self.validate_ownership_type(value):
-            raise ValueError("")
+            raise ValueError("Invalid ownership type.")
         self.__ownership_type = value
 
     @property
@@ -68,81 +134,43 @@ class ClientBase:
     @address.setter
     def address(self, value: str):
         if not self.validate_address(value):
-            raise ValueError("Неподходящий адрес")
+            raise ValueError("Invalid address.")
         self.__address = value
-
-    @property
-    def phone(self):
-        return self.__phone
-
-    @phone.setter
-    def phone(self, value: str):
-        if not self.validate_phone(value):
-            raise ValueError("Неподходящий номер телефона")
-        self.__phone = value
-
-    @classmethod
-    def create_new_client(cls, client_id: int, name: str, ownership_type: str, address: str, phone: str):
-        return cls(client_id=client_id, name=name, ownership_type=ownership_type, address=address, phone=phone)
-
 
 
     def __str__(self):
         return (f"Client {self.name}, ID: {self.client_id}, Ownership Type: {self.ownership_type}, "
                 f"Address: {self.address}, Phone: {self.phone}")
 
-    def __eq__(self, other):
-        if not isinstance(other, ClientBase):
-            return False
-        return self.phone == other.phone
 
+class ClientBrief(ClientBase):
 
-    @staticmethod
-    def from_string(client_str: str):
-        parts = client_str.split(',')
-        if len(parts) != 5:
-            raise ValueError("The string must contain 5 comma-separated elements.")
-        client_id = int(parts[0].strip())
-        name, ownership_type, address, phone = map(str.strip, parts[1:])
-        return ClientBase(client_id, name, ownership_type, address, phone)
+    def __init__(self, client_id: int, name: str, phone: str):
+        super().__init__(client_id, name, phone)
 
     @classmethod
-    def from_json(cls, json_str: str):
-        data = json.loads(json_str)
+    def from_client(cls, client: Client):
+        if not isinstance(client, Client):
+            raise TypeError("Argument must be an instance of Client.")
         return cls(
-            client_id=data.get('client_id'),
-            name=data['name'],
-            ownership_type=data['ownership_type'],
-            address=data['address'],
-            phone=data['phone']
+            client_id=client.client_id,
+            name=client.name,
+            phone=client.phone
         )
 
-    def to_json(self) -> str:
-        return json.dumps({
-            'client_id': self.client_id,
-            'name': self.name,
-            'ownership_type': self.ownership_type,
-            'address': self.address,
-            'phone': self.phone
-        }, ensure_ascii=False, indent=4)
-
-    def to_dict(self) -> dict:
-        return {
-            'client_id': self.client_id,
-            'name': self.name,
-            'ownership_type': self.ownership_type,
-            'address': self.address,
-            'phone': self.phone
-        }
-
+    def __str__(self):
+        return f"Client Brief: {self.name}, ID: {self.client_id}, Phone: {self.phone}"
 
 if __name__ == "__main__":
-    client = ClientBase(
+    client = Client(
         client_id=1,
         name="ООО Ромашка",
         ownership_type="ООО",
         address="г. Москва, ул. Ленина, д.10",
-        phone="8 (123) 456-78-90"
+        phone="8 (123) 456-78-90",
     )
 
-    print(ClientBase.validate_name("wwww"))
+    print(client)
+
+    client_brief = ClientBrief.from_client(client)
+    print(client_brief)
